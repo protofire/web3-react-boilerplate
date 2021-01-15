d22b3cc2ca7fc4fe965bc33d55ef0142e4e7f2f0
Alaa Saleh <photonite@users.noreply.github.com>
2021-01-15 14:52:54 -0300

    Add typescript, update packages and install useful packages (#1)

--- src/connectors.js
+++
@@ -1,7 +0,0 @@
-import { Connectors } from 'web3-react'
-
-const { InjectedConnector } = Connectors
-
-const MetaMask = new InjectedConnector({ supportedNetworks: [4] })
-
-export const connectors = { MetaMask }
